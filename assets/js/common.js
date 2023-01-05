53770075fc54aa56c0690656cd944bb07218aa4b
Miho Horiuchi <paglinawan0520@gmail.com>
2023-01-05 10:19:35 +0900

    Update ローカルで確認できるように更新

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,22 +1,46 @@
-// スムーススクロール
-$(function () {
-  $('a[href^="#"]').on('click', function () {
-    const speed = 500;
-    const href = $(this).attr("href");
-    const target = $(href == "#" || href == "" ? 'html' : href);
-    const position = target.offset().top;
-    $("html, body").animate({
-      scrollTop: position
-    }, speed, "swing");
-    return false;
-  });
-});
+jQuery(function ($) {
+  // スクロールイベント
+  $(function () {
+    scrollToggleClass();
+    $(window).on('scroll', function () {
+      scrollToggleClass();
+    });
 
-// ハンバーガーメニュー
-$(function () {
-  let ham = $('#js-ham');
-  ham.click(function () {
-    $(this).add('body').toggleClass('is-active');
+    function scrollToggleClass() {
+      $('.js-scroll').each(function () {
+        let $target = $(this).offset().top;
+        let $scroll = $(window).scrollTop();
+        let $windowHeight = $(window).height();
+        if ($scroll > $target - $windowHeight + 80) {
+          $(this).addClass('is-active');
+        } else {
+          $(this).removeClass('is-active');
+        }
+      });
+    }
   });
+
+  // スムーススクロール
+  $(function () {
+    $('a[href^="#"]').on('click', function () {
+      const $speed = 500;
+      const $href = $(this).attr('href');
+      const $target = $($href == '#' || $href == '' ? 'html' : $href);
+      const $position = $target.offset().top;
+      $('html, body').animate({
+        scrollTop: $position
+      }, $speed, 'swing');
+      return false;
+    });
+  });
+  
+  // ハンバーガーメニュー
+  $(function () {
+    let $ham = $('#js-ham');
+    $ham.on('click', function () {
+      $(this).add('body').toggleClass('is-active');
+    });
+  });
+  
 });
 
--- footer.php
+++
@@ -1,7 +0,0 @@
-<?php include('inc/footer.php'); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="/assets/js/common.js"></script>
-
-</body>
-
-</html>
